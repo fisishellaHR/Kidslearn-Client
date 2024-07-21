f3f85a1ec8b4a9f02f42d05caf477be1f5ec6b6f
fisi <hegihayagi34@gmail.com>
2024-07-21 17:33:44 +0700

    trash email localstorage

--- src/Layout-views/Admin-Views/Dashboard/Header.jsx
+++ src/Layout-views/Admin-Views/Dashboard/Header.jsx
@@ -15,6 +15,7 @@ export default function Header() {
       );
       alert("Berhasil Logout!");
       localStorage.removeItem("username");
+      localStorage.removeItem("email");
       setTimeout(() => {
         navigate("/loginadmin");
       }, 2000);
