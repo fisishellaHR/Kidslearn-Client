a9dc80104aab72e75210ca76e81d31882086bf3a
fisi <hegihayagi34@gmail.com>
2024-09-12 19:06:53 +0700

    pasti bisa sih ini

--- src/Layout-views/Admin-Views/NilaiPengguna/NilaiPengguna.jsx
+++ src/Layout-views/Admin-Views/NilaiPengguna/NilaiPengguna.jsx
@@ -10,12 +10,46 @@ const NilaiPengguna = () => {
         const response = await axios.get(
           "https://kidslearn-server.vercel.app/api/auth/getUsers"
         );
-        const uniqueUsers = Array.from(
-          new Set(response.data.map((user) => user.userId))
-        ).map((userId) => {
-          return response.data.find((user) => user.userId === userId);
-        });
-        setUsers(uniqueUsers);
+
+        // Mengelompokkan data berdasarkan userId
+        const groupedUsers = response.data.reduce((acc, userRecord) => {
+          const {
+            userId,
+            userName,
+            userEmail,
+            title,
+            passGrade,
+            historyAnswer,
+            experiment,
+            score,
+          } = userRecord;
+
+          if (!acc[userId]) {
+            acc[userId] = {
+              userId,
+              userName,
+              userEmail,
+              quizzes: [],
+            };
+          }
+
+          acc[userId].quizzes.push({
+            title: title || "No Title",
+            passGrade: passGrade || "No PassGrade",
+            historyAnswer: historyAnswer.map((quizItem) => ({
+              questionId: quizItem.questionId,
+              answer: quizItem.answer,
+            })),
+            experiment,
+            score,
+          });
+
+          return acc;
+        }, {});
+
+        // Mengubah object menjadi array
+        const result = Object.values(groupedUsers);
+        setUsers(result);
       } catch (error) {
         console.error("Error fetching users:", error);
       }
@@ -32,6 +66,9 @@ const NilaiPengguna = () => {
             <th className="px-6 py-3 text-left text-lg font-extrabold text-primary uppercase tracking-wider border-r border-primary">
               User
             </th>
+            <th className="px-6 py-3 text-left text-lg font-extrabold text-primary uppercase tracking-wider border-r border-primary">
+              Email
+            </th>
             <th className="px-6 py-3 text-left text-lg font-extrabold text-primary uppercase tracking-wider">
               Nilai
             </th>
@@ -44,17 +81,17 @@ const NilaiPengguna = () => {
                 <td className="px-6 py-4 whitespace-nowrap text-base text-white border-r border-primary">
                   {user.userName}
                 </td>
+                <td className="px-6 py-4 whitespace-nowrap text-base text-white border-r border-primary">
+                  {user.userEmail}
+                </td>
                 <td className="px-6 py-4 whitespace-nowrap text-base text-white">
                   <ul>
-                    {Array.isArray(user.historyAnswer) &&
-                    user.historyAnswer.length > 0 ? (
-                      user.historyAnswer.map((answerItem, index) => (
-                        <li key={index + 1} className="flex">
+                    {user.quizzes.length > 0 ? (
+                      user.quizzes.map((quiz, index) => (
+                        <li key={index} className="flex">
                           <p className="px-2">
-                            {user.title
-                              ? `${user.title} Percobaan ke ${index + 1} : ${
-                                  user.score
-                                }`
+                            {quiz.title
+                              ? `${quiz.title} Percobaan ke ${quiz.experiment} : ${quiz.score}`
                               : "No Title"}
                           </p>
                         </li>
@@ -68,7 +105,7 @@ const NilaiPengguna = () => {
             ))
           ) : (
             <tr>
-              <td colSpan="2" className="text-center text-white">
+              <td colSpan="3" className="text-center text-white">
                 Tidak ada data
               </td>
             </tr>
